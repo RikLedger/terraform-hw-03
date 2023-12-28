63378eecd53e5703cb4d0b0a5db0c16546f624d4
RikLedger <131041498+RikLedger@users.noreply.github.com>
2023-12-28 22:54:48 +0300

    Update disk_vm.tf

--- src/disk_vm.tf
+++ src/disk_vm.tf
@@ -1,44 +1,50 @@
-resource "yandex_compute_disk" "disk_vm" {
-  count = 3
-  name = "${"disk"}-${count.index}"
-  size = 1
+resource "yandex_compute_disk" "default_disk" {
+  count      = 3
+  name       = "default-disk-${count.index + 1}"
+  type       = "network-hdd"
+  zone       = var.default_zone
+  size       = 5
+  block_size = 4096
 }
 
-resource "yandex_compute_instance" "storage" {
-  count = 1
-  name        = "storage-${count.index+1}"
-  depends_on = [yandex_compute_disk.disk_vm]
+resource "yandex_compute_instance" "storage_server" {
+
+  depends_on = [yandex_compute_disk.default_disk]
+
+  name        = "storage"
   platform_id = "standard-v1"
+
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm_base.cores
+    memory        = var.vm_base.memory
+    core_fraction = var.vm_base.core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
 
-dynamic "secondary_disk" {
-  for_each = yandex_compute_disk.disk_vm[*].id
-  content {
-    disk_id = secondary_disk.value
+  dynamic "secondary_disk" {
+    for_each = toset(yandex_compute_disk.default_disk[*].id)
+    content {
+      disk_id     = secondary_disk.key
+      auto_delete = true
+    }
   }
-}
+
+  metadata = local.ssh_keys_and_serial_port
 
   scheduling_policy {
     preemptible = true
   }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
-    security_group_ids = [yandex_vpc_security_group.example.id]
   }
 
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = local.ssh
-  }
+  allow_stopping_for_update = true
 
 }
