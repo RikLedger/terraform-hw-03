e54bed81a9ab3027fa5acc535d5fe0d79ef8fd5f
RikLedger <131041498+RikLedger@users.noreply.github.com>
2023-12-28 22:55:36 +0300

    Update for_each-vm.tf

--- src/for_each-vm.tf
+++ src/for_each-vm.tf
@@ -1,21 +1,18 @@
-resource "yandex_compute_instance" "for_each" {
-  depends_on = [yandex_compute_instance.count]
-  for_each = {
-    main = var.each_vm[0]
-    replica = var.each_vm[1]
-  }
-  name        = "${each.key}"
+resource "yandex_compute_instance" "frontend_server" {
+
+  for_each = local.virtual_machines
+
+  name        = each.value.vm_name
   platform_id = "standard-v1"
   resources {
-    cores         = "${each.value.cpu}"
-    memory        = "${each.value.ram}"
-    core_fraction = 5
+    cores         = each.value.vm_cpu
+    memory        = each.value.vm_ram
+    core_fraction = each.value.vm_core_fraction
   }
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
-      type = "network-hdd"
-      size = "${each.value.disk}"
+      size     = each.value.vm_disk_size
     }
   }
   scheduling_policy {
@@ -24,12 +21,11 @@ resource "yandex_compute_instance" "for_each" {
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
-    security_group_ids = [yandex_vpc_security_group.example.id]
   }
+  metadata = local.ssh_keys_and_serial_port
 
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = local.ssh
-  }
+  depends_on = [
+    yandex_compute_instance.backend_server
+  ]
 
 }
