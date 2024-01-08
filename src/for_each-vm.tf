9ebdc334347eccff8d76a945e4dddfd42fa6d4f9
RikLedger <131041498+RikLedger@users.noreply.github.com>
2024-01-08 19:30:12 +0300

    Update for_each-vm.tf

--- src/for_each-vm.tf
+++ src/for_each-vm.tf
@@ -1,31 +1,72 @@
-resource "yandex_compute_instance" "frontend_server" {
+variable "os_image" {
+  type = string
+  default = "ubuntu-2004-lts"
+}
 
-  for_each = local.virtual_machines
+data "yandex_compute_image" "ubuntu" {
+  family = var.os_image
+}
+variable "vm_resources" {
+  type = list(object({
+    vm_name = string
+    cpu     = number
+    ram     = number
+    disk    = number
+    platform_id = string
+  }))
+  default = [
+    {
+      vm_name = "main"
+      cpu     = 2
+      ram     = 2
+      disk    = 5
+      platform_id = "standard-v1"
+    },
+    {
+      vm_name = "replica"
+      cpu     = 2
+      ram     = 2
+      disk    = 10
+      platform_id = "standard-v1"
+    },
+  ]
+}
 
-  name        = each.value.vm_name
-  platform_id = "standard-v1"
+locals {
+  ssh-keys = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "yandex_compute_instance" "for_each" {
+  depends_on = [yandex_compute_instance.web]
+  for_each = { for i in var.vm_resources : i.vm_name => i }
+  name          = each.value.vm_name
+
+  platform_id = each.value.platform_id
   resources {
-    cores         = each.value.vm_cpu
-    memory        = each.value.vm_ram
-    core_fraction = each.value.vm_core_fraction
+    cores         = each.value.cpu
+    memory        = each.value.ram
+
   }
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
-      size     = each.value.vm_disk_size
+      size = each.value.disk
     }
+  }
+
+    metadata = {
+    ssh-keys = "ubuntu:${local.ssh-keys}"
+    serial-port-enable = "1"
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+    security_group_ids = [
+      yandex_vpc_security_group.example.id
+    ]
   }
   scheduling_policy {
     preemptible = true
   }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
-  }
-  metadata = local.ssh_keys_and_serial_port
-
-  depends_on = [
-    yandex_compute_instance.backend_server
-  ]
-
 }
