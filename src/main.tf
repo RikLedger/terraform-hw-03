ec5804e75e62000ba30bf402963a16e4b0ca8450
RikLedger <131041498+RikLedger@users.noreply.github.com>
2024-01-08 19:34:07 +0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -7,20 +7,3 @@ resource "yandex_vpc_subnet" "develop" {
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
 }
-
-resource "null_resource" "web_hosts_provision" {
-depends_on = [yandex_compute_instance.count, yandex_compute_instance.for_each, yandex_compute_instance.storage]
-
-
-  provisioner "local-exec" {
-    command  = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/inventory.yml ${abspath(path.module)}/test.yml"
-    on_failure = continue
-    environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
-  }
-    triggers = {
-      always_run         = "${timestamp()}"
-      playbook_src_hash  = file("${abspath(path.module)}/test.yml")
-      ssh_public_key     = local.ssh
-    }
-
-}
